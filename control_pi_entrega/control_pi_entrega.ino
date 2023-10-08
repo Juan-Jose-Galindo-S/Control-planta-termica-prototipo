3b05020cb46dfce796874af487c5a1f2776aa461
Juan-Jose-Galindo-S <147337725+Juan-Jose-Galindo-S@users.noreply.github.com>
2023-10-08 12:33:26 -0500

    Add files via upload

---
+++ control_pi_entrega/control_pi_entrega.ino
@@ -0,0 +1,103 @@
+//datos recibidos labview
+char reflab="";
+
+String refst;
+
+long int refint;
+//Acción de Control
+float u=0.0;
+float u_1=0.0; 
+float e=0.0;
+float e_1=0.0;
+float ref=0.0;
+float s0=1.2138;  
+float s1=-1.1844;
+
+// sensor
+int voltaje;
+float valor;
+unsigned long tiempo;
+float tiempo2;
+String cadena = "";
+String dato = "";
+String resta = "";
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  pinMode(9,OUTPUT);
+
+}
+
+
+void loop() {
+  if(Serial.available())
+  {
+    reflab=Serial.read();
+    refst+=reflab;
+    if(reflab=='\n')
+    {
+    refint=refst.toInt();
+    ref=refint-10000;
+    ref=ref/100;
+    ref=ref/10;
+
+   
+  tiempo = millis();
+  tiempo2 = (float)tiempo / 1000.0;
+  voltaje = analogRead(A0);
+  
+  valor = voltaje * (5.0 / 1023.0);
+  
+    //Serial.println(cadena);
+    
+  e=(ref-valor);
+  // Controle PID
+  u = s0*100*e + s1*100*e_1 + u_1 ; //Ley del controlador PID discreto
+  //Retorno a los valores reales
+  e_1=e;
+  u_1=u;
+ 
+ 
+//Serial.println("error");
+//Serial.println(e);
+//resta = String(ref)+"-"+String(valor);
+//Serial.println("resta");
+//Serial.println(resta);
+//Serial.println("control");
+//
+if(u < 0.0){
+      
+      analogWrite(9,0);
+      u_1=0.0;
+    }
+
+    else if(u > 11){
+    
+      analogWrite(9,map(u, 11, 25, 100, 255));
+     
+     u_1=2;
+    }
+    
+    else
+    {
+      analogWrite(9,map(u, 0, 10, 20, 255));
+     
+    }
+    cadena = String(ref*100)+"|"+String(valor*100)+"|"+String(e*1000)+"|"+String(u*100);
+     Serial.println(cadena);
+//    Serial.println(ref);
+//    Serial.println(",");
+//    Serial.println(valor);
+//    Serial.println(",");
+//    Serial.println(e);
+//    Serial.println(",");
+//    Serial.println(u);
+  
+    delay(50);
+    refst="";
+    }
+  }
+}
